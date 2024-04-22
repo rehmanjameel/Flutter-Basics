9460e33bff5d0f2ebbb48de6d9246fb97512d46c
rehmanjameel <abdulrehmancs17@gmail.com>
2024-04-22 14:18:25 +0500

    changes in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:first_app/gradient_container.dart';
 
+void dices(int a) {
+  print(a);
+}
+
 void main() {
+  dices(12);
   //Material app is a main widget as it's a starting point
   runApp(
     const MaterialApp(
